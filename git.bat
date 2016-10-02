954b342be3a8f3e86e095794f252e55b132ac24a
Conrad Thieme <conradthieme@web.de>
2016-10-02 10:22:56 +0200

    testing

--- git.bat
+++ git.bat
@@ -2,5 +2,6 @@
 
 sudo git add .
 #get commitComment from input
+#set /p commitComment=Enter comment for commit: 
 sudo git commit evolution.py #-m commitComment
 sudo git push https://github.com/ConTcoN/Evolution
