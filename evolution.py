7cda17d1f8106153881559fc9f150555be0dcb40
Conrad Thieme <conradthieme@web.de>
2016-10-03 20:09:28 +0200

    only minor changes

--- evolution.py
+++ evolution.py
@@ -1,19 +1,19 @@
-# population, creature, generation
+# population, creature, generation, species
 
 #%%
-
-##
 
 class Creature:
     def __init__(self, numberFingers, numberGeneration):
         self.numberFingers = numberFingers
         self.numberGeneration = numberGeneration
         
-    
 
 #%%
+
 if __name__ == "__main__":
     paul = Creature(5)
     maggy = Creature(4)
     paggy = Creature(min(paul.numberFingers, maggy.numberFingers))
     print(paggy.numberFingers)
+    
+
