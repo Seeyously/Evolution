4ae32660c2bc03dd7dbdd534de4ea78d7b268425
Conrad Thieme <conradthieme@web.de>
2016-10-14 17:55:08 +0200

    added content to module

--- generation.py
+++ generation.py
@@ -1,1 +1,14 @@
-#class Generation
+#!/usr/bin env python3
+
+class Generation:
+    #numberGenerations = 0
+    
+    def __init__(self):
+        global numberGenerations
+        self.number = numberGenerations
+        #print(self.number)
+        numberGenerations += 1
+        self.creatures = []
+    
+    def addCreature(self, creature):
+        self.creatures.append(creature)
