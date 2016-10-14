3d5e027b493e30b6dc641c287209af548d6aeb95
Conrad Thieme <conradthieme@web.de>
2016-10-14 17:55:18 +0200

    added content to module

--- time.py
+++ time.py
@@ -1,1 +1,12 @@
-#class Time
+#!/usr/bin env python3
+
+class Time:
+    def __init__(self):
+        self.currentTime = 0
+    
+    def up(self):
+        self.currentTime += 1
+        
+    def down(self):
+        if self.currentTime > 0:
+            self.currentTime -= 1
