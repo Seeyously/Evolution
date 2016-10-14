2fbc2c2b69f29021de2b579a9117d5bff4daa59f
Conrad Thieme <conradthieme@web.de>
2016-10-14 17:54:56 +0200

    added content to module

--- creature.py
+++ creature.py
@@ -1,1 +1,15 @@
-#class Creature
+#!/usr/bin/env python3
+
+class Creature:
+    
+    def __init__(self, numberFingers, generation):
+        self.numberFingers = numberFingers
+        self.generation = generation
+        self.age = 0
+        self.size = 1
+    
+    def changeAge(self, deltaAge):
+        self.age += deltaAge
+        
+    def rise(self, deltaSize):
+        self.size += deltaSize
