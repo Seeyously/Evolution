d245b2ecc69e595238b83829b12af30ceeccf9e9
Conrad Thieme <conradthieme@web.de>
2016-10-18 23:14:55 +0200

    first commit

--- world.py
+++ world.py
@@ -1,35 +1,27 @@
 #%%
-import tkinter as tk
 
-class ExampleApp(tk.Tk):
-    def __init__(self):
-        tk.Tk.__init__(self)
-        t = SimpleTable(self, 10,10)
-        t.pack(side="top", fill="x")
-        t.set(1,1,5)
-        #t.lift()
+class World():
+    def __init__(self, width = 10, height = 10):
+        self.width = width
+        self.height = height
+        self.world = [[0 for h in range(self.height)] for w in range(self.width)]
 
-class SimpleTable(tk.Frame):
-    def __init__(self, parent, rows=10, columns=2):
-        tk.Frame.__init__(self, parent, background="black")
-        self._widgets = []
-        for row in range(rows):
-            current_row = []
-            for column in range(columns):
-                label = tk.Label(self, borderwidth = 0, width = 2, height = 1)
-                label.grid(row=row, column=column, sticky="nsew", padx=1, pady=1)
-                current_row.append(label)
-            self._widgets.append(current_row)
+    def setPosition(self, x, y, value):
+        try:
+            #x in range(self.width)
+            self.world[x][y] = value
+        except:
+            print("x should be between 0 and " + str(self.width-1))
+            print("y should be between 0 and " + str(self.height-1))
+        else:
+            self.world[x][y] = value
 
-        for column in range(columns):
-            self.grid_columnconfigure(column, weight=1)
-
-
-    def set(self, row, column, value):
-        widget = self._widgets[row][column]
-        widget.configure(text=value)
-
-if __name__ == "__main__":
-    app = ExampleApp()
-    app.attributes('-topmost', True)
-    app.mainloop()
+    def show(self):
+        print(self.world)
+        
+if __name__ == '__main__':
+    myWorld = World()
+    myWorld.setPosition(0,0,10)
+    myWorld.show()
+         
+    
